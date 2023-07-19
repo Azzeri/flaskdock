dfbeb80711ccc3bd67275e785b0ad3cef4c3197f
Mariusz Waloszczyk <azmario2698@gmail.com>
2023-07-19 20:55:57 +0200

    search rows limited

--- app/solr_connector.py
+++ app/solr_connector.py
@@ -7,12 +7,15 @@ class SolrConnector:
         self.solr_port = 8983
         self.solr_url = f"http://solr:{self.solr_port}/solr/{self.solr_core}"
         self.solr_instance = self.get_solr_instance()
+        self.no_results = 150
 
     def get_solr_instance(self):
         return Solr(self.solr_url, always_commit=True)
 
     def searchByKeywords(self, query):
-        searchResults = self.solr_instance.search(f"keywords:{query}")
+        searchResults = self.solr_instance.search(
+            f"keywords:{query}", rows=self.no_results
+        )
         preparedResults = []
         for index, result in enumerate(searchResults, start=1):
             preparedResults.append(
